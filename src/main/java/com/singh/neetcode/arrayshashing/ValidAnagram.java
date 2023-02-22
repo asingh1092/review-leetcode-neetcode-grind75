a7d500369e77150f8668bff404296d21ae453286
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2023-02-21 21:04:07 -0500

    Valid anagram wIP

---
+++ src/main/java/com/singh/neetcode/arrayshashing/ValidAnagram.java
@@ -0,0 +1,20 @@
+package com.singh.neetcode.arrayshashing;
+
+public class ValidAnagram {
+
+    /*
+       Given two strings s and T, return true if t is an anagram of s, and false otherwise.
+
+       An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+       typically using all the original letters exactly once.
+
+       Base case -> same length
+       1. String -> char arrays -> sort -> iterate over
+       2. int[256]
+
+     */
+    public boolean isAnagram(String s, String t) {
+        Integer.valueOf('c');
+        return false;
+    }
+}
