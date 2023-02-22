48bfe59c51fe3705c7e547860b6bbf336b775e8d
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2023-02-21 20:56:09 -0500

    contains dups using a hashset

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # neetcode
 needcode.io problems
+
+# This repo's purpose is to grind out neetcode problems for review purposes.
+https://neetcode.io/roadmap
---
+++ src/main/java/com/singh/neetcode/arrayshashing/ContainsDuplicates.java
@@ -0,0 +1,37 @@
+package com.singh.neetcode.arrayshashing;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicates {
+
+    /*
+       Given an integer array nums, return true if any value appears at least twice in the array, and return
+       false if every element is distinct
+
+        Could use a hashset to see if we have any dups
+        Time: O(n) where n is number of integers in nums
+        Space: O(n)
+
+       How do we do all this in place????
+            Sorting -> n O(log(n))
+
+     */
+
+    public static boolean containsDups(int[] nums) {
+        Set<Integer> hSet = new HashSet<Integer>();
+        for (int num : nums) {
+            if (hSet.contains(num)) {
+                return true;
+            } else {
+                hSet.add(num);
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(containsDups(new int[]{1, 2, 3, 1}));
+    }
+
+}
