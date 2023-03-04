be16907758690a4fef192f70535de9642e1320da
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2023-03-04 15:48:23 -0500

    design a min stack

---
+++ src/main/java/com/singh/neetcode/stack/StackNode.java
@@ -0,0 +1,31 @@
+package com.singh.neetcode.stack;
+
+public class StackNode {
+
+    public int value;
+    public StackNode next;
+
+    public StackNode() {}
+
+    public StackNode(int value) {
+        this.value = value;
+    }
+
+    public StackNode(StackNode next) {
+        this.next = next;
+    }
+
+    public StackNode(int value, StackNode next) {
+        this.value = value;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "StackNode{" +
+                "value=" + value +
+                ", next=" + next +
+                '}';
+    }
+
+}
