9eb0ed051d898bb5dae610d074c57a9850409cc8
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2023-03-15 20:04:20 -0400

    merged sorted linked lists optimized

---
+++ src/main/java/com/singh/neetcode/linkedlist/MergeTwoSortedLists.java
@@ -0,0 +1,57 @@
+package com.singh.neetcode.linkedlist;
+
+import com.singh.testing.linkedlist.ListNode;
+
+public class MergeTwoSortedLists {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode merged = new ListNode();
+        ListNode dummy = merged;
+        while (list1 != null && list2 != null) {
+            if (list1.value < list2.value) {
+//                merged.next = new ListNode(list1.value); // dont need to be new list nodes
+                merged.next = list1;
+                list1 = list1.next;
+            } else {
+//                merged.next = new ListNode(list2.value);
+                merged.next = list2;
+                list2 = list2.next;
+            }
+            merged = merged.next;
+        }
+//
+//        while (list1 != null) {
+//            merged.next = list1;
+//            list1 = list1.next;
+//            merged = merged.next;
+//        }
+//
+//        while (list2 != null) {
+//            merged.next = list2;
+//            list2 = list2.next;
+//            merged = merged.next;
+//        }
+
+        if (list1 != null) {
+            merged.next = list1;
+        } else if (list2 != null) {
+            merged.next = list2;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4, null)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4, null)));
+        MergeTwoSortedLists merge = new MergeTwoSortedLists();
+        System.out.println(merge.mergeTwoLists(list1, list2));
+        list1 = new ListNode();
+        list2 = new ListNode();
+        System.out.println(merge.mergeTwoLists(list1, list2));
+        list1 = new ListNode();
+        list2 = new ListNode(32);
+        System.out.println(merge.mergeTwoLists(list1, list2));
+    }
+
+}
