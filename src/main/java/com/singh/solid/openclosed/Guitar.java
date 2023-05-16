630414573b0c20fabe2f26c3b8785d889fa439ed
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2023-05-15 23:33:32 -0400

    open closed example

---
+++ src/main/java/com/singh/solid/openclosed/Guitar.java
@@ -0,0 +1,11 @@
+package com.singh.solid.openclosed;
+
+public class Guitar {
+
+    private String make;
+    private String model;
+    private int volume;
+
+    // Constructors, getters, setters
+
+}
---
+++ src/main/java/com/singh/solid/openclosed/SuperCoolGuitarWithFlames.java
@@ -0,0 +1,11 @@
+package com.singh.solid.openclosed;
+
+public class SuperCoolGuitarWithFlames extends Guitar {
+
+    // instead of changing existing guitar class, we can extend it to add additional modifications so we don't break
+    // existing code
+    private String flameColor;
+
+    // Constructors, getters, setters
+
+}
