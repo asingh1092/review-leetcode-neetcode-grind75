b2f3721acb426052e2e036239c1536d3cf86ab8d
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2023-05-15 22:39:34 -0400

    started int to roman and finished combination sum using backtracking.

--- src/main/java/com/singh/random/CombinationSum.java
+++ src/main/java/com/singh/random/CombinationSum.java
@@ -1,2 +1,56 @@
-package com.singh.random;public class CombinationSum {
+package com.singh.random;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CombinationSum {
+    /*
+        Given an array of distinct integers candidates and a target integer target, return a list of all unique
+        combinations of candidates where the chosen numbers sum to target. you may return the combinations in any
+        order.
+
+        The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the
+        frequency of at least one of the chosen numbers is different.
+
+        The test cases are generated such that the number of unique combinations that sum up to target is less than
+        150 combinations for the given input.
+
+        e.g. candidates = [2,3,6,7], target = 7 -> [[2,2,3],[7]]
+
+            [2,3,5] target = 8
+            [[2,2,2,2],[2,3,3],[3,5]]
+
+                    2               3       6  7
+                 2    3 6 7
+              2  3      6  7
+            2
+     */
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ret = new ArrayList<>();
+        // Arrays.sort(candidates);
+        backtrack(candidates, ret, new ArrayList<>(), target, 0);
+        return ret;
+    }
+
+    private void backtrack(int[] input, List<List<Integer>> ret, ArrayList<Integer> tempList, int remain, int start) {
+        if (remain < 0) {
+            return;
+        } else if (remain == 0) {
+            ret.add(new ArrayList<>(tempList));
+        } else { // remain > 0
+            for (int i = start; i < input.length; i++) {
+                tempList.add(input[i]);
+                backtrack(input, ret, tempList, remain - input[i], i); // start is i here and not i + 1 because we can use the same num many times
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        CombinationSum cs = new CombinationSum();
+        System.out.println(cs.combinationSum(new int[]{2,3,7}, 7));
+        System.out.println(cs.combinationSum(new int[]{2,3,5}, 8));
+    }
 }
--- src/main/java/com/singh/random/IntegerToRoman.java
+++ src/main/java/com/singh/random/IntegerToRoman.java
@@ -1,2 +1,24 @@
-package com.singh.random;public class IntegerToRoman {
+package com.singh.random;
+
+import java.util.HashMap;
+
+public class IntegerToRoman {
+    public String intToRoman(int num) {
+        String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] hrns = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] ths = {"", "M", "MM", "MMM"};
+
+        return ths[num / 1000] + hrns[(num % 1000) / 100] + tens[(num % 100) / 10] + ones[num % 10];
+    }
+
+    public static void main(String[] args) {
+        IntegerToRoman itr = new IntegerToRoman();
+        System.out.println(itr.intToRoman(1));
+        System.out.println(itr.intToRoman(3));
+        System.out.println(itr.intToRoman(58));
+        System.out.println(itr.intToRoman(1994));
+        System.out.println(itr.intToRoman(3999));
+        System.out.println(itr.intToRoman(4000));
+    }
 }
