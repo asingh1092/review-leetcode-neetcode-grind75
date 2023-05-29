e20bac24edc191a360d121fa968c074a89fe3725
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2023-05-29 17:43:33 -0400

    max depth)

--- src/main/java/com/singh/neetcode/trees/easy/MaximumDepthOfBinaryTree.java
+++ src/main/java/com/singh/neetcode/trees/easy/MaximumDepthOfBinaryTree.java
@@ -1,14 +1,21 @@
-package com.singh.neetcode.trees;
+package com.singh.neetcode.trees.easy;
 
 import com.singh.testing.binarytree.TreeNode;
 
 public class MaximumDepthOfBinaryTree {
-    /*
+
+   /*
+                               5
+                    3                      3
+                5       7             7         5
+            4         2     8      8     2          4
+
         Given the root of a binary tree, return its maximum depth
 
         A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the
         farthest leaf node.
-     */
+
+    */
 
     public int maxDepth(TreeNode root) {
         // Base Condition
@@ -19,4 +26,5 @@ public class MaximumDepthOfBinaryTree {
         // Induction
         return Math.max(left, right) + 1;
     }
+
 }
