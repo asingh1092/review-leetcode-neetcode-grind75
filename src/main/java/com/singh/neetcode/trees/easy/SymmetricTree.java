4d56b6d006f62defde16f97ac1e63e7cb96b943a
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2023-05-29 16:55:33 -0400

    same tree and started symmetric tree

---
+++ src/main/java/com/singh/neetcode/trees/easy/SymmetricTree.java
@@ -0,0 +1,28 @@
+package com.singh.neetcode.trees.easy;
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class SymmetricTree {
+
+    /*
+
+
+
+     */
+
+
+}
---
+++ src/main/java/com/singh/testing/binarytree/BFS.java
@@ -0,0 +1,14 @@
+package com.singh.testing.binarytree;
+
+public class BFS {
+
+    // binary, or in general -> n-ary
+    // full = all nodes have two children except for leaves
+    // perfect = full binary tree where all the leaves are at the same depth and all parent nodes have two children
+    // complete = every node is fulled except possibly the last  and to the left node
+    // balanced = height left sub-tree and right sub-tree is equal or differs by most 1
+    // BST = binary search tree = bianry search trees are used for various searching and sorting algos
+    //                          -> everything on the left is less than the parent, and value of the right node is greater
+
+
+}
