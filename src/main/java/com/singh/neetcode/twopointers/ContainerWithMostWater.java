90c7b4cd7fe4e8618b572ef9ed228ff5cf94e944
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2023-08-30 22:36:06 -0400

    init container problem

---
+++ src/main/java/com/singh/neetcode/twopointers/ContainerWithMostWater.java
@@ -0,0 +1,28 @@
+package com.singh.neetcode.twopointers;
+
+public class ContainerWithMostWater {
+    /*
+        Given an integer array height of length n.
+        There are n vertical lines drawn such that the two endpoints of the ith line are (i,0) and (i, height[i]).
+
+        Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+        Return the maximum amount of water a container can store: NO SLANT CONTAINERS
+
+        [ 1, 8, 6, 2, 5, 4, 8, 3, 7 ]
+                ^  ^
+        Brute force approach: calculate every area and spit out the max
+        Space: O(1)
+        Time: O(n+(n-1)+(n-2)+(n-3)..+ 1)) = n*(n-1)/2 = n^2
+
+     */
+
+    public static int maxWaterinContainer(int[] height) {
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
