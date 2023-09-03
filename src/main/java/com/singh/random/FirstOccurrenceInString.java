b68d970b1f601b95723a656d5a3fc7b23a285ca1
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2023-09-03 19:31:07 -0400

    first non unique char

---
+++ src/main/java/com/singh/random/FirstOccurrenceInString.java
@@ -0,0 +1,28 @@
+package com.singh.random;
+
+public class FirstOccurrenceInString {
+
+    /*
+            statistics -> a
+              ^
+            babylon -> a
+     */
+
+    public static Character findFirstNonUniqueChar(String s) {
+        int[] cache = new int[26];
+        for (char c : s.toCharArray()) {
+            cache[c - 'a']++;
+        }
+        for (char c : s.toCharArray()) {
+            if (cache[c - 'a'] <= 1) {
+                return c;
+            }
+        }
+        return 'f';
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findFirstNonUniqueChar("statistics"));
+        System.out.println(findFirstNonUniqueChar("geeksforgeeks"));
+    }
+}
