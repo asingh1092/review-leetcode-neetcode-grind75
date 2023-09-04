76164902a7796a0576888e7408e522db0e8d0865
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2023-09-04 16:56:27 -0400

    longest word in string sentence

---
+++ src/main/java/com/singh/random/LongestWord.java
@@ -0,0 +1,41 @@
+package com.singh.random;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LongestWord {
+
+    public static String findLongestWordinString(String sen) {
+        List<String> words = new ArrayList<>();
+        char[] cArray = sen.toCharArray();
+        int begin = 0;
+        int end = 0;
+        for (char c : cArray) {
+             if (Character.isAlphabetic(c)) {
+                 end++;
+             } else {
+                 words.add(sen.substring(begin, end));
+                 begin = end + 1;
+                 end = begin;
+             }
+        }
+        words.add(sen.substring(begin, end));
+
+        int longestIndex = 0;
+        int pointer = 0;
+        int max = 0;
+        for (String word : words) {
+            if (word.length() > max) {
+                max = word.length();
+                longestIndex = pointer;
+            }
+            pointer++;
+        }
+        return words.get(longestIndex);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findLongestWordinString("fun&!! time"));
+        System.out.println(findLongestWordinString("I love dogs"));
+    }
+}
