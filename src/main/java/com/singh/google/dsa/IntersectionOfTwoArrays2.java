829dbd2ea9d916573a65d65f894076e83ba71ddf
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2023-09-06 22:35:27 -0400

    first unique char in string and intersection of two arrays

---
+++ src/main/java/com/singh/google/dsa/IntersectionOfTwoArrays2.java
@@ -0,0 +1,38 @@
+package com.singh.google.dsa;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class IntersectionOfTwoArrays2 {
+
+
+    public int[] intersect(int[] nums1, int[] nums2) {
+        // first make a count map of each num in nums1 <Integer, freq
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int i : nums1) {
+            if (freqMap.containsKey(i)) {
+                freqMap.put(i, freqMap.get(i) + 1);
+            } else {
+                freqMap.put(i, 1);
+            }
+        }
+
+        List<Integer> ret = new ArrayList<>();
+        // second go through nums2 and see if num exists in count map, remove 1 count, and add to list to be returned
+        for (int j : nums2) {
+            if (freqMap.containsKey(j) && freqMap.get(j) > 0) {
+                ret.add(j);
+                freqMap.put(j, freqMap.get(j) - 1);
+            }
+        }
+
+        int[] realRet = new int[ret.size()];
+        for (int i = 0; i < realRet.length; i++) {
+            realRet[i] = ret.get(i);
+        }
+
+        return realRet;
+    }
+}
