94054eeebfc86b99e215825400f02e930a58b2a5
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2023-09-06 23:28:37 -0400

    redo group anagrams

---
+++ src/main/java/com/singh/google/dsa/mapsdicts/keydesign/GroupAnagrams.java
@@ -0,0 +1,40 @@
+package com.singh.google.dsa.mapsdicts.keydesign;
+
+import java.util.*;
+
+public class GroupAnagrams {
+
+    /*
+        each letter has a integer value so add them up to equal some key then add the word to its list which is the value
+
+     */
+
+    public static List<List<String>> anagrams(String[] strs) {
+        // Map of <Key (chars), List<Words>>
+        Map<String, List<String>> map = new HashMap<>();
+        for (String word : strs) {
+            char[] ca = new char[26];
+            for (char c : word.toCharArray()) {
+                ca[c - 'a']++;
+            }
+            String key = String.valueOf(ca);
+
+            if (map.containsKey(key)) {
+                List<String> list = map.get(key);
+                list.add(word);
+                map.put(key, list);
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(word);
+                map.put(key, list);
+            }
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan","ate", "nat", "bat"};
+        String[] strs2 = {"cab","tin","pew","duh","may","ill","buy","bar","max","doc"};
+        System.out.println(anagrams(strs2));
+    }
+}
