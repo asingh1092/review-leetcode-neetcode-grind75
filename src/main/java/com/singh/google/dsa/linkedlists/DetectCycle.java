3ef0063f3a0508e08116c1bc9f27fe63e8e9c4cf
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2023-09-08 13:55:45 -0400

    check for cycle and detect where cycle begins

---
+++ src/main/java/com/singh/google/dsa/linkedlists/DetectCycle.java
@@ -0,0 +1,27 @@
+package com.singh.google.dsa.linkedlists;
+
+public class DetectCycle {
+    public static ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode pointer = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        while (pointer != slow) {
+            pointer = pointer.next;
+            slow = slow.next;
+        }
+        return pointer;
+    }
+}
--- src/main/java/com/singh/google/dsa/linkedlists/LinkedListCycle.java
+++ src/main/java/com/singh/google/dsa/linkedlists/LinkedListCycle.java
@@ -10,9 +10,12 @@ public class LinkedListCycle {
 
      */
     public static boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
         ListNode slow = head;
         ListNode fast = head;
-        while (fast != null || fast.next != null) {
+        while (fast.next != null || fast.next.next != null) {
             slow = slow.next;
             fast = fast.next.next;
             if (slow == fast) {
