3bb13e2df4f6b08ad231b72e3a2d3e3ca9421333
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2023-09-10 17:34:34 -0400

    same tree

--- src/main/java/com/singh/google/dsa/trees/SameTree.java
+++ src/main/java/com/singh/google/dsa/trees/SameTree.java
@@ -7,9 +7,29 @@ public class SameTree {
 
         Two binary trees are considered the same if they are structurally identical and the nodes have the same value
 
-        PRE ORDER:
-        IN ORDER:
-        POST ORDER:
+        PRE ORDER: root, left, right
+        IN ORDER: left, root, right
+        POST ORDER: left, right, root
+
+        preOrder traversal -> check root/parent of both to see if they are same, then go down left of both, then right
      */
+
+    public static boolean isSameTree(BinaryTreeNode p, BinaryTreeNode q) {
+        if(p == null && q == null) {
+            return true;
+        }
+        if(p == null || q == null) {
+            return false;
+        }
+        if(p.val == q.val)
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return false;
+    }
+
+    public static void main(String[] args) {
+        BinaryTreeNode p = new BinaryTreeNode(1, new BinaryTreeNode(2), null);
+        BinaryTreeNode q = new BinaryTreeNode(1, null, new BinaryTreeNode(2));
+        System.out.println(isSameTree(p, q));
+    }
 
 }
