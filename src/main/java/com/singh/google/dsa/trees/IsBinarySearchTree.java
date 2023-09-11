e98d8b6a179a4240914af94d22ed901e596a4650
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2023-09-10 20:22:27 -0400

    bst validity

---
+++ src/main/java/com/singh/google/dsa/trees/IsBinarySearchTree.java
@@ -0,0 +1,31 @@
+package com.singh.google.dsa.trees;
+
+public class IsBinarySearchTree {
+
+    /*
+        Given the root, check if the tree is a binary search tree
+            BST is..
+                * the data value of every node in a node's left subtree is less than the data value of the that node
+                * the data value of every node in a node's right subtree is greater than the data value of that node
+                * the data value of every node is distinct
+
+                3
+            2       4
+        1       5       6
+
+        pre: root -> left, right
+        in: left -> root -> right
+        post: left, right, root
+
+     */
+
+    public static boolean isValidBST(BinaryTreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public static boolean isValidBST(BinaryTreeNode root, long minVal, long maxVal) {
+        if (root == null) return true;
+        if (root.val >= maxVal || root.val <= minVal) return false;
+        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
+    }
+}
