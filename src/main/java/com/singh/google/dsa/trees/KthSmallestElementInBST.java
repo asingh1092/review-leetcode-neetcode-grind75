b85bf1e9e9eeee696e4f733c2c5c0399edf293cc
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2023-09-10 20:32:55 -0400

    Return kth smallest node

---
+++ src/main/java/com/singh/google/dsa/trees/KthSmallestElementInBST.java
@@ -0,0 +1,24 @@
+package com.singh.google.dsa.trees;
+
+import java.util.Stack;
+
+public class KthSmallestElementInBST {
+
+    // iterative process
+    public static BinaryTreeNode kthSmallestElement(BinaryTreeNode root, int k) {
+        if (root == null) {
+            return null;
+        }
+        Stack<BinaryTreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (--k == 0) break;
+            root = root.right;
+        }
+        return root;
+    }
+}
