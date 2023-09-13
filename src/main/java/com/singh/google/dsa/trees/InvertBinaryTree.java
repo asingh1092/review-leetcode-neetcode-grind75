a93fa8c8f295b189042080a7a3e2542baf9c38ee
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2023-09-12 21:31:01 -0400

    bombed merge two sorted lsits

---
+++ src/main/java/com/singh/google/dsa/trees/InvertBinaryTree.java
@@ -0,0 +1,17 @@
+package com.singh.google.dsa.trees;
+
+public class InvertBinaryTree {
+
+    public static BinaryTreeNode invertTree(BinaryTreeNode root) {
+        if (root != null) {
+            // do something to left
+            BinaryTreeNode left = invertTree(root.left);
+            // do something to right
+            BinaryTreeNode right = invertTree(root.right);
+            root.right = left;
+            root.left = right;
+
+        }
+        return root;
+    }
+}
