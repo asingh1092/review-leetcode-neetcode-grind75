173de6bed7568e75bb5fc906aa853c626b4b3304
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2023-09-13 18:41:41 -0400

    binary search on 2D array

--- src/main/java/com/singh/neetcode/binarysearch/SearchA2DMatrix.java
+++ src/main/java/com/singh/neetcode/binarysearch/SearchA2DMatrix.java
@@ -3,34 +3,55 @@ package com.singh.neetcode.binarysearch;
 public class SearchA2DMatrix {
 
     /*
-        Given an [m x n] integer matrix 'matrix' with the following properties:
-            * Each row is sorted in non-drecreasing order.
-            * The first integer of each row is greater than the last integer of the previous row.
-        Given an integer target, return true if target is in matrix or false otherwise.
-        You must write a solution in O(log(m*n)) time complexity.
-                0   1   2   3
-             0  1   3   5   7
-             1 10  11  16  20
-             2 23  30  34  60
+        Given an m x n integer matrix [matrix] with the follow two props:
+            1. Each row is sorted in non-decreasing order
+            2. The 1st integer of each row is greater than the last integer of the previous row
+
+        Given an integer target, return true if target is in matrix or false otherwise
+
+        Must write algo in O(log(m*n))
+         0   1   2    3
+       0 1   3   5    7
+       1 10  11  16  20
+       2 23  30  34  60
+
+        matrix = [row][col]
      */
 
-    public boolean searchMatrix(int[][] matrix, int target) {
+    public static boolean searchMatrix(int[][] matrix, int target) {
         int row = 0;
-        int rowSize = matrix[0].length;
         int col = 0;
-        int colSize = matrix.length;
-        int mid = colSize - row;
-        while (row < colSize) {
-            // check
+        int rowSize = matrix.length;
+        int colSize = matrix[0].length;
+        int rowHigh = rowSize - 1;
+        int colHigh = colSize - 1;
+        while (row <= rowHigh) {
+            int midRow = (rowHigh - row) / 2 + row;
+            if (target >= matrix[midRow][col] && target <= matrix[midRow][colHigh]) {
+                // check row now
+                while (col <= colHigh) {
+                    int midCol = (colHigh - col) / 2 + col;
+                    if (target == matrix[midRow][midCol]) {
+                        return true;
+                    } else if (target < matrix[midRow][midCol]) {
+                        colHigh = midCol - 1;
+                    } else {
+                        col = midCol + 1;
+                    }
+                }
+            } else if (target < matrix[midRow][col]) {
+                rowHigh = midRow - 1;
+            } else {
+                row = midRow + 1;
+            }
+
         }
         return false;
     }
 
     public static void main(String[] args) {
-        int[][] matrix = new int[][]{
-                {1,3,6, 7},
-                {10, 11, 16, 20},
-                {23, 30, 34, 60}
-        };
+        int[][] matrix = new int[][]{{1,3,5,7}, {10,11,16,20}, {23,30,34,60}};
+        System.out.println(searchMatrix(matrix, 9));
+        System.out.println(searchMatrix(matrix, 20));
     }
 }
