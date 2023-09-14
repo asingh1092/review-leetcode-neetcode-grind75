9a21bf7430db2f52b99fe7da47628f4188c71fdf
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2023-09-14 15:21:32 -0400

    binary tree level order traversal

--- src/main/java/com/singh/neetcode/trees/medium/BinaryTreeLevelOrderTraversal.java
+++ src/main/java/com/singh/neetcode/trees/medium/BinaryTreeLevelOrderTraversal.java
@@ -1,38 +1,59 @@
 package com.singh.neetcode.trees.medium;
 
+import com.singh.google.dsa.trees.BinaryTreeNode;
 import com.singh.testing.binarytree.TreeNode;
 
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
 
 public class BinaryTreeLevelOrderTraversal {
 
-    // using BFS
-    public List<List<Integer>> levelOrder(TreeNode root) {
+    /*
+         Given the root of a binary tree, return the level order traversal of its nodes' values (i.e from left to right, level by level)
+
+                3
+
+            9      20
+
+              15         7
+
+          = [[3],[3,20],[15,7]
+
+          // use iterative / recursive BFS
+
+     */
+
+    // iterative
+    public List<List<Integer>> levelOrder(BinaryTreeNode root) {
         List<List<Integer>> ret = new ArrayList<>();
-        Queue<TreeNode> queue = new LinkedList<>();
+        Queue<BinaryTreeNode> queue = new ArrayDeque<>();
+        if (root == null) {
+            return ret;
+        }
         queue.add(root);
         while (!queue.isEmpty()) {
-            List<Integer> level = new ArrayList<>();
-            int count = queue.size();
-            for (int i = 0; i < count; i++) {
-                TreeNode node = queue.poll();
+            List<Integer> newLevel = new ArrayList<>();
+            int nodesInLevel = queue.size();
+            for (int i = 0; i < nodesInLevel; i++) {
+                BinaryTreeNode node = queue.poll();
                 if (node != null) {
-                    level.add(node.val);
+                    newLevel.add(node.val);
                     if (node.left != null) {
                         queue.add(node.left);
                     }
-                    if (node.right !=null) {
+                    if (node.right != null) {
                         queue.add(node.right);
                     }
                 }
             }
-            ret.add(level);
-
+            ret.add(newLevel);
         }
         return ret;
     }
 
-    // DFS
+    // DFS using recursion
     public List<List<Integer>> levelOrderDFS(TreeNode root) {
         List<List<Integer>> ret = new ArrayList<>();
         dfsHelper(ret, root, 0);
@@ -45,8 +66,7 @@ public class BinaryTreeLevelOrderTraversal {
             ret.add(new ArrayList<>());
         }
         ret.get(height).add(root.val);
-        dfsHelper(ret, root.left, height+1);
-        dfsHelper(ret, root.right, height+1);
+        dfsHelper(ret, root.left, height + 1);
+        dfsHelper(ret, root.right, height + 1);
     }
-
 }
