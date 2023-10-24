e457aafff6a99130ebabe9a9f415bb4ef069be37
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2023-10-23 23:53:32 -0400

    contains dups

--- src/main/java/com/singh/grind/ContainsDuplicate.java
+++ src/main/java/com/singh/grind/ContainsDuplicate.java
@@ -1,2 +1,19 @@
-package com.singh.grind;public class ContainsDuplicate {
+package com.singh.grind;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            if (set.contains(n)) {
+                return true;
+            } else {
+                set.add(n);
+            }
+        }
+        return false;
+    }
 }
