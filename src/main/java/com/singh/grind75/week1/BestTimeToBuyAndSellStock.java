2ab8fa7dc01e6de74ffd98b915eaa72f36a86c74
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2024-04-15 22:33:44 -0400

    three sum

--- src/main/java/com/singh/grind75/week1/BestTimeToBuyAndSellStock.java
+++ src/main/java/com/singh/grind75/week1/BestTimeToBuyAndSellStock.java
@@ -1,2 +1,34 @@
-package com.singh.grind75.week1;public class BestTimeToBuyAndSellStock {
+package com.singh.grind75.week1;
+
+public class BestTimeToBuyAndSellStock {
+
+    /*
+        7 1 5 3 6 4
+
+
+     */
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        int profit;
+        int left = 0;
+        int right = 1;
+        while (right < prices.length) {
+            if (prices[left] < prices[right]) {
+                profit = prices[right] - prices[left];
+                if (profit > max) {
+                    max = profit;
+                }
+            } else {
+                left = right;
+            }
+            right++;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = new int[]{7, 1, 5, 3, 6, 4};
+        BestTimeToBuyAndSellStock buyAndSellStock = new BestTimeToBuyAndSellStock();
+        System.out.println(buyAndSellStock.maxProfit(prices));
+    }
 }
