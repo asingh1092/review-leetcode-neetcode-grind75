d9ef6a9a9d7633a9daded14ff2722c28a57f113c
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2024-04-15 23:02:09 -0400

    majority element

---
+++ src/main/java/com/singh/grind75/week1/MajorityElement.java
@@ -0,0 +1,31 @@
+package com.singh.grind75.week1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement {
+
+    public int majorityElement(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int majorityThreshold = nums.length / 2;
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int n : nums) {
+            if (freq.containsKey(n) && freq.get(n) + 1 > majorityThreshold) {
+                return n;
+            } else {
+                freq.put(n, freq.getOrDefault(n, 0) + 1);
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        MajorityElement element = new MajorityElement();
+        int[] nums = new int[]{3, 2, 3};
+        System.out.println(element.majorityElement(nums));
+        nums = new int[]{2, 2, 1, 1, 1, 2, 2};
+        System.out.println(element.majorityElement(nums));
+    }
+}
