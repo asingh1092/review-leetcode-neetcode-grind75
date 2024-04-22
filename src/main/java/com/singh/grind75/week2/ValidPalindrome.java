6e36954f37db4c0e6fbe1435268fb80c66467f72
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2024-04-21 21:40:32 -0400

    valid palindrome

---
+++ src/main/java/com/singh/grind75/week2/ValidPalindrome.java
@@ -0,0 +1,32 @@
+package com.singh.grind75.week2;
+
+public class ValidPalindrome {
+
+    /*
+        A man, a plan, a canal: Panama
+        ^                           ^
+
+     */
+    public boolean isValid(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+        char[] ca = s.toCharArray();
+        while (start < end) {
+            char st = ca[start];
+            char en = ca[end];
+            if (!Character.isLetterOrDigit(st)) {
+                start++;
+            } else if(!Character.isLetterOrDigit(en)) {
+                end--;
+            } else {
+                if (Character.toLowerCase(st) == Character.toLowerCase(en)) {
+                    start++;
+                    end--;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
