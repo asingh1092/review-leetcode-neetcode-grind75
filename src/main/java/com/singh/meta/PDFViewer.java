b7857f20b290e79a9c11a28dabb535b610e5c419
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2024-04-26 19:35:39 -0400

    pdf viewr

---
+++ src/main/java/com/singh/meta/PDFViewer.java
@@ -0,0 +1,16 @@
+package com.singh.meta;
+
+import java.util.*;
+
+public class PDFViewer {
+
+    public int maxHeightOfView(List<Integer> h, String word) {
+        int max = 1;
+        for (char c : word.toCharArray()) {
+            max = Math.max(h.get(c - 'a'), max);
+        }
+        return max * word.length();
+    }
+
+
+}
