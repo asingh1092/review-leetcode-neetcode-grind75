373ba34587011d286c94df2f8cd7f16026e57160
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2025-02-26 00:07:46 -0500

    max abs sum of any subarray (Start)

---
+++ src/main/java/com/singh/leetcodedaily/MaximumAbsoluteSumOfAnySubarray.java
@@ -0,0 +1,16 @@
+package com.singh.leetcodedaily;
+
+public class MaximumAbsoluteSumOfAnySubarray {
+
+    /*
+        Given an integer array nums, find the max abs sum of any subarray of nums
+        e.g.
+        nums = [1,-3,2,3,-4] -> [2,3] as abs(2+3) = 5
+                ^
+
+        nums = [2,-5,1,-4,3,-2] -> [-5,1,-4] as abs(-5+1-4) = abs(-8) = 8
+     */
+    public int maxAbsoluteSum(int[] nums) {
+        return -1;
+    }
+}
